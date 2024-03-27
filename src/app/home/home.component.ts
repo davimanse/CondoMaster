8e817cfee3a3484c91255df21c95955d6322c39d
davimanse <125286260+davimanse@users.noreply.github.com>
2024-03-27 10:21:41 +0100

    fetchrecord LIST not finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { PocketBaseService } from '../servizi/fetch-records.service';
 import { CommonModule } from '@angular/common';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+  condoService = inject(PocketBaseService);
+  condomini !: any;
   constructor(private authService: AuthService, private router: Router, private pbservice:PocketBaseService) { }
-  data : any;
-  async ngOnInit() {
-    this.data = await this.pbservice.fetchCondominio();
-    console.log(this.data);
+  
+  
+  private async loadCondomini() {
+    try{
+      this.condomini = await this.condoService.fetchCondominio();
+    }catch(e){
+      console.log(e);
+    }
+
   }
+ ngOnInit():void{
+   this.loadCondomini();
+  }
+
 
   logout() {
     this.authService.logout();
