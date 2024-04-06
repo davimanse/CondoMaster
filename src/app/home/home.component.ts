dfedb92fb61d6839f358b7cd68a647bb2d335e2f
simonepanfilii <104305887+simonepanfilii@users.noreply.github.com>
2024-04-06 08:21:40 +0200

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,8 +19,22 @@ export class HomeComponent implements OnInit {
     private pbservice: PocketBaseService
   ) {}
   
+<<<<<<< Updated upstream
   async ngOnInit() {
     this.condomini = await this.pbservice.fetchCondominio( );
+=======
+  condomini!: CondoModel[];
+  private async LoadCondo() {
+    try {
+
+      this.condomini = await this.CondominioService.getCondomini();
+      this.condomini = this.condomini.filter(condo => condo.IDAdmin === this.adminId);
+      console.log("davide");      
+    }
+    catch (error) {
+      console.log(error);
+    }
+>>>>>>> Stashed changes
   }
 
   logout() {
