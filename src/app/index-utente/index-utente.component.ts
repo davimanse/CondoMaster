8fa3882ae67fdee7621070705d485ab1205c5ea7
simonepanfilii <104305887+simonepanfilii@users.noreply.github.com>
2024-05-15 08:10:22 +0200

    1.1

--- src/app/index-utente/index-utente.component.ts
+++ src/app/index-utente/index-utente.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-index-utente',
   templateUrl: './index-utente.component.html',
   styleUrl: './index-utente.component.scss'
 })
-export class IndexUtenteComponent {
+export class IndexUtenteComponent implements OnInit{
+  ngOnInit()
+  {
+    
+  }
 
 }
